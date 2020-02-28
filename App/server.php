80962b7761bbe172a7e3118a5221c2c7f6565cd9
Hernan Pinos <hernan.pinos@outlook.com>
2020-02-28 17:21:17 -0500

    CREACION DEL ARREGLO DE LIBROS CON SUS DATOS CORRESPODIENTES

--- App/server.php
+++ App/server.php
@@ -7,16 +7,40 @@ $allowedResourceTypes = [
     'genres',
 ];
 
-//CREACION DE UNA VARIABLE PARA CONSULTAR DESDE LA URL
+//Validamos que el recurso este disponible
 $resourceType = $_GET['resource_type'];
 //IN_ARRAY VERIFICAR QUE ESTE EN UN ARREGLO
 if(!in_array($resourceType,$allowedResourceTypes)){
     die;
 }
 
+//Defini los resusos
+$books = [
+    1 => [
+        'titulo' => 'Lo que el viento se llevo',
+        'id_autor' => 1,
+        'id_genero' => 1,
+    ],
+
+    2 => [
+        'titulo' => 'La Iliada',
+        'id_autor' => 2,
+        'id_genero' => 2,
+    ],
+
+    3 => [
+        'titulo' => 'La Odisea',
+        'id_autor' => 3,
+        'id_genero' => 3,
+    ],
+
+];
+
+header('Content-Type: application/json');
+//Generamos la respuesta asumiendo que el pedido es correcto
 switch(strtoupper($_SERVER['REQUEST_METHOD'])){
     case 'GET':
-        # code...
+        echo json_encode($books);
         break;
     case 'POST':
         # code...
@@ -28,3 +52,4 @@ switch(strtoupper($_SERVER['REQUEST_METHOD'])){
         # code...
         break;
 }
+

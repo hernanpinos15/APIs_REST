9150ce294ab42f89101f33eb9d6e15164f4c6474
HERNAN PINOS <hernan.pinos@outlook.com>
2020-05-19 12:49:54 -0500

    Exponer un recurso en particular a traves de HTTP GET

--- App/server.php
+++ App/server.php
@@ -1,5 +1,7 @@
 <?php
 
+//Ejecutar el servidor con el siguiente comando (Ubicarse en la ruta del archivo) >php -S localhost:8000 <nombre del archivo>
+
 //CREACION DE UN ARREGLO DE TIPOS DE RECURSOS
 $allowedResourceTypes = [
     'books',
@@ -37,10 +39,20 @@ $books = [
 ];
 
 header('Content-Type: application/json');
+
+//Levantamos el id del recurso buscado
+$resourceId = array_key_exists('resource_id', $_GET) ? $_GET['resource_id'] : '';
+
 //Generamos la respuesta asumiendo que el pedido es correcto
 switch(strtoupper($_SERVER['REQUEST_METHOD'])){
     case 'GET':
-        echo json_encode($books);
+        if( empty($resourceId)){
+            echo json_encode($books);
+        }else{
+            if(array_key_exists($resourceId, $books)){
+                echo json_encode($books[$resourceId]);
+            }
+        }
         break;
     case 'POST':
         # code...
